bdad25ec3012cd8fd1cab20a8f1828a1925d5d92
Artur Kharisov <93016855+JustArtur@users.noreply.github.com>
2022-04-24 02:09:04 +0500

    :wq
    some updates and create delete method

--- avl_tree.py
+++ avl_tree.py
@@ -1,115 +1,112 @@
 class Node:
-    def __init__(self, value=None, left=None, right=None):
+    def __init__(self, value=None, left=None, right=None, parent=None):
         self.left = left
         self.right = right
         self.value = value
-        self.parent = None
-        self.height = 1
+        self.parent = parent
 
 
 class Tree:
     def __init__(self):
         self.root = None
-        self.max_height = 0
-        self.count = 0
+
+    def __str__(self):
+        return self.value
 
     def insert(self, value):
-        if self.root == None: self.root = Node(value)
+        # init the tree
+        if self.root == None:
+            self.root = Node(value)
+            return
         self._insert(value, self.root)
 
     def _insert(self, value, cur_node):
-        # print(f"Insert {value}")
+        # if the value is already exist, then just return
         if value == cur_node.value:
-            print('This value already exist')  # not return, because recursion
             return
-        elif value < cur_node.value:  # checking where to go
-            print("Going left")
+        # Checking where to go right or left
+        elif value < cur_node.value:
             if cur_node.left == None:
-                self.count += 1
                 cur_node.left = Node(value)
-                cur_node.left.height += self.count
-                self.max_height = max(self.max_height, cur_node.left.height)
-                print("Linked node")
+                cur_node.left.parent = cur_node
             else:
-                print("Recursion")
                 self._insert(value, cur_node.left)
-        elif value > cur_node.value:  # checking where to go
-            print("Going right")
+        else:
             if cur_node.right == None:
-                self.count += 1
                 cur_node.right = Node(value)
-                cur_node.right.height += self.count
-                self.max_height = max(self.max_height, cur_node.right.height)
-                print("Linked node")
+                cur_node.right.parent = cur_node
             else:
-                print("Recursion")
                 self._insert(value, cur_node.right)
 
     def delete(self, value):
-        cur_node = self.root
-        while True:
-            if value < cur_node.value:  # checking where to go
-                print("Going left", cur_node.value)
-                self._delete(value, cur_node.left)
-            elif value > cur_node.value:  # checking where to go
-                print("Going right", cur_node.value)
-                self._delete(value, cur_node.right)
+        self._delete(self.find(value))
 
-    # def _delete(self, value, cur_node):
-    #     if value < cur_node.value:  # checking where to go
-    #         print("Going left", cur_node.value)
-    #         self.__delete(value, cur_node.left)
-    #     elif value > cur_node.value:  # checking where to go
-    #         print("Going right", cur_node.value)
-    #         self.__delete(value, cur_node.right)
+    def _delete(self, cur_node):
+        # return the rightmost node of left child
+        def __get_rightmost_node(node):
+            while node.right != None:
+                node = node.right
+            return node
 
-    def _delete(self, value, cur_node):
-        if cur_node == None:
-            print("Can't find value")
-            return
-        elif cur_node.value == value:
-            print("Match")
-            # If the node doesn't have a right and left subtree, then just cut it off and exit from the method
-            if cur_node.left != None and cur_node.right != None:
-                print("Deleting")
-                cur_node = None
-                return
-            # If the node have a right and left subtree, then applying a special algorithm.
-            elif cur_node.left != None and not cur_node.right != None:
-                print("Swapping with both subtree")
-                cur_node.value, cur_node.left.value = cur_node.left.value, cur_node.value
-                return
-                # If condition above fails, then we check for a left or right subtree
-                # and simply reassign the links between the Nodes
-            elif cur_node.left != None:  # Check for the left subtree
-                print("Deleting")
-                cur_node = cur_node.left
-                return
-            elif  cur_node.right != None:  # Check for the right subtree
-                print("Deleting")
-                cur_node = cur_node.right
+        def __delete_successor(node):
+            pass
+
+        # If the node doesn't have a right and left subtree, then just cut it off and exit from the method
+        if cur_node.left == None and cur_node.right == None:
+            if cur_node.parent.left == cur_node:
+                cur_node.parent.left = None
+            else:
+                cur_node.parent.right = None
+            print("Deleting", 'all none')
+
+        # If the node have a right and left subtree, then applying a special algorithm.
+        elif cur_node.left != None and cur_node.right != None:
+            print("Both subtree")
+            right_node = __get_rightmost_node(cur_node.left)
+            cur_node.value = right_node.value
+            parent_node = right_node.parent
+            if parent_node.left == right_node:
+                parent_node.left = None
+            else:
+                parent_node.right = None
+
+
+            # If condition above fails, then we check for a left or right subtree
+            # and simply reassign the links between the Nodes
+        elif cur_node.left != None:
+            print("Deleting")
+            if cur_node.parent.left == cur_node:
+                cur_node.parent.left = cur_node.left
+            else:
+                cur_node.parent.right = cur_node.left
         else:
-            self._delete(value, cur_node)
+            print("Deleting")
+            if cur_node.parent.left == cur_node:
+                cur_node.parent.left = cur_node.right
+            else:
+                cur_node.parent.right = cur_node.right
+            cur_node.parent = cur_node.right
 
     def find(self, value):
         cur_node = self.root
         while True:
-            if cur_node == None: return False
-            elif cur_node.value == value: return True
-            elif value < cur_node.value:  # checking where to go
-                # print("Going left", cur_node.value)
+            if cur_node == None:
+                return False
+            # if value is equal, then return True
+            elif cur_node.value == value:
+                return cur_node
+            # Checking where to go right or left
+            elif value < cur_node.value:
                 cur_node = cur_node.left
-                continue
-            elif value > cur_node.value:  # checking where to go
-                # print("Going right", cur_node.value)
+            else:
                 cur_node = cur_node.right
 
+
     def left_rotate(self):
-        pass
+         pass
 
     def right_rotate(self):
         pass
-
 
     # def __repr__(self):
     #     cur_nodes = [self.root]
@@ -137,18 +134,12 @@ class Tree:
     #         next_nodes = []
 
 
-
-
-
 t = Tree()
 t.insert(10)
 t.insert(5)
-t.insert(6)
-t.insert(4)
-t.insert(15)
-t.insert(12)
-t.insert(16)
-t.insert(11)
+t.insert(2)
 t.insert(3)
-# repr(t)
-print(t.find(11))
+t.insert(4)
+t.insert(6)
+t.delete(5)
+print(t.find(5))
